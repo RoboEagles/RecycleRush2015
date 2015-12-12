b5bb57a46bb6d867593aaec89ed87e1c7ded8b98
Jaden Bottemiller <jadenbottemiller@gmail.com>
2015-12-12 11:22:13 -0800

    Vector class

--- RecycleRush4/RecycleRush4/src/com/RoboEagles4579/math/Vector3d.java
+++ RecycleRush4/RecycleRush4/src/com/RoboEagles4579/math/Vector3d.java
@@ -1,13 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package com.RoboEagles4579.math;
 
-public class Vector3d {
-	
-	public double X, Y, Z;
-	
-	public Vector3d(double x, double y, double z) {
-		this.X = x;
-		this.Y = y;
-		this.Z = z;
-	}
-
+/**
+ *
+ * @author bstevens
+ */
+public class Vector3d extends Vector2d {
+    
+    public double Z;
+    
+    public Vector3d(double x, double y, double z) {
+        super(x,y);
+        this.Z = z;
+    }
+    
+    public double magnitude() {//Returns magnitude of the vector
+        return Math.sqrt(X*X + Y*Y + Z*Z);
+    }
+    
+    public double angleXZ() {
+        return Math.tan(X / Z);
+    }
+    
+    public double angleYZ() {
+        return Math.tan(Y / Z);
+    }
+    
+    public void reset() {
+        X = 0;
+        Y = 0;
+        Z = 0;
+    }
+    
 }
