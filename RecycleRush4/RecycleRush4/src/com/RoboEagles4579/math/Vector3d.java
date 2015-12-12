e8ef7c1fcd18b31a8e51bb3d650da5a1fe39f50c
Jaden Bottemiller <jadenbottemiller@gmail.com>
2015-12-11 18:44:25 -0800

    Added Vector3d to main packages

---
+++ RecycleRush4/RecycleRush4/src/com/RoboEagles4579/math/Vector3d.java
@@ -0,0 +1,13 @@
+package com.RoboEagles4579.math;
+
+public class Vector3d {
+	
+	public double X, Y, Z;
+	
+	public Vector3d(double x, double y, double z) {
+		this.X = x;
+		this.Y = y;
+		this.Z = z;
+	}
+
+}
